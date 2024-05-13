9fa6bcec1ce8d30b4f72395e3e5f8711a3f0b175
ankit-tailor <tailorankit3345@gmail.com>
2024-05-13 19:07:23 +0530

    feat: next app

---
+++ base-template/apps/next/app/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Button } from "@base-template/ui/button";
+
+import styles from "../styles/index.module.css";
+
+export default function Web() {
+  return (
+    <div className={styles.container}>
+      <h1>Web</h1>
+      <Button onClick={() => console.log("Pressed!")} text="Boop" />
+    </div>
+  );
+}
