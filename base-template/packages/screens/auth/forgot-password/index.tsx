9209a4ce422d24d7aea63bfdc5b260b2fadb3874
Riya <riyas@geekyants.com>
2024-05-15 17:48:17 +0530

    fix:fixes in forgot-password screen

--- base-template/packages/screens/auth/forgot-password/index.tsx
+++ base-template/packages/screens/auth/forgot-password/index.tsx
@@ -143,8 +143,8 @@ const ForgotPasswordScreen = () => {
       </VStack>
 
       <VStack
-        className="md:items-center md:justify-center w-full max-w-[440px] p-9 md:m-auto md:w-1/2"
-        space="2xl"
+        className="md:items-center md:justify-center w-full max-w-[440px] p-9 gap-16 md:gap-10 md:m-auto md:w-1/2"
+        space="3xl"
       >
         <VStack className="md:items-center" space="xs">
           <Icon
@@ -155,13 +155,12 @@ const ForgotPasswordScreen = () => {
           <Heading className="md:text-center" size="3xl">
             Forgot Password?
           </Heading>
-          <Text>
-            Enter email ID associated with your account to get the restoration
-            link.
+          <Text className="text-sm">
+            Enter email ID associated with your account.
           </Text>
         </VStack>
 
-        <VStack space="xl" className="w-full">
+        <VStack space="xl" className="w-full ">
           <FormControl isInvalid={!!errors?.email} className="w-full">
             <FormControlLabel>
               <FormControlLabelText>Email</FormControlLabelText>
@@ -200,9 +199,6 @@ const ForgotPasswordScreen = () => {
               </FormControlErrorText>
             </FormControlError>
           </FormControl>
-          {/* Label Message */}
-        </VStack>
-        <VStack className="w-full" space="lg">
           <Button className="w-full" onPress={handleSubmit(onSubmit)}>
             <ButtonText className="font-medium">Send Link</ButtonText>
           </Button>
