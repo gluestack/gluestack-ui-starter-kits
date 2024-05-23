15d0e22ef1752dec7a916da3ecbb6777c95683f3
ankit-tailor <tailorankit3345@gmail.com>
2024-05-23 12:50:31 +0530

    feat: starter kit restructure

--- starter-kit/packages/components/ui/grid/styles.jsx
+++ starter-kit/packages/components/ui/grid/styles.jsx
@@ -2,8 +2,8 @@ import { tva } from "@gluestack-ui/nativewind-utils/tva";
 import { isWeb } from "@gluestack-ui/nativewind-utils/IsWeb";
 const gridItemBaseStyle = isWeb ? "w-auto" : "";
 export const gridStyle = tva({
-  base: `w-full box-border flex-row flex-wrap justify-start grid grid-cols-12`,
+    base: `w-full box-border flex-row flex-wrap justify-start grid`,
 });
 export const gridItemStyle = tva({
-  base: `w-full col-span-1 ${gridItemBaseStyle}`,
+    base: `w-full col-span-1 ${gridItemBaseStyle}`,
 });
