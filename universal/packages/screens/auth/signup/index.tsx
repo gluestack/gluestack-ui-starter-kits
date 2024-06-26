8de4412bdd107272016b84119c1c8698cd759fa6
sra1kumar-NULL <sravansandeep163@gmail.com>
2024-06-26 13:34:56 +0530

    fix:added fix for universal:next stylings

--- universal/packages/screens/auth/signup/index.tsx
+++ universal/packages/screens/auth/signup/index.tsx
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
-import { SafeAreaView } from "@/components/ui/safe-area-view";
 import { Toast, ToastTitle, useToast } from "@/components/ui/toast";
 import { HStack } from "@/components/ui/hstack";
 import { VStack } from "@/components/ui/vstack";
 import { Heading } from "@/components/ui/heading";
 import { Text } from "@/components/ui/text";
 import { LinkText } from "@/components/ui/link";
-import { Image } from "@/components/ui/image";
 import Link from "@unitools/link";
 import {
   FormControl,
@@ -39,6 +37,7 @@ import { AlertTriangle } from "lucide-react-native";
 import { GoogleIcon } from "./assets/icons/google";
 import { Pressable } from "@/components/ui/pressable";
 import useRouter from "@unitools/router";
+import { AuthLayout } from "../layout";
 
 const signUpSchema = z.object({
   email: z.string().min(1, "Email is required").email(),
@@ -65,38 +64,6 @@ const signUpSchema = z.object({
   rememberme: z.boolean().optional(),
 });
 type SignUpSchemaType = z.infer<typeof signUpSchema>;
-
-type AuthLayoutProps = {
-  children: React.ReactNode;
-};
-
-const AuthLayout = (props: AuthLayoutProps) => {
-  return (
-    <SafeAreaView className="w-full h-full">
-      <HStack className="w-full h-full bg-background-0">
-        <VStack
-          className="relative w-0 hidden md:flex md:h-full md:min-w-[50%] items-center  justify-center p-7"
-          space="md"
-        >
-          <Image
-            source={require("@/assets/auth/radialGradient.png")}
-            className="h-full w-full absolute inset-0 -z-10"
-            alt="Radial Gradient"
-          />
-          <Image
-            source={require("@/assets/auth/logo.png")}
-            className="h-40 w-40"
-            alt="Gluestack Logo"
-          />
-        </VStack>
-
-        <VStack className="md:items-center md:justify-center w-full md:max-w-[440px] p-9 md:gap-10 gap-16 md:m-auto md:w-1/2">
-          {props.children}
-        </VStack>
-      </HStack>
-    </SafeAreaView>
-  );
-};
 
 const SignUpWithLeftBackground = () => {
   const {
@@ -154,7 +121,7 @@ const SignUpWithLeftBackground = () => {
   };
   const router = useRouter();
   return (
-    <>
+    <VStack className="max-w-[440px] w-full" space="md">
       <VStack className="md:items-center" space="md">
         <Pressable
           onPress={() => {
@@ -361,7 +328,7 @@ const SignUpWithLeftBackground = () => {
           </Link>
         </HStack>
       </VStack>
-    </>
+    </VStack>
   );
 };
 
