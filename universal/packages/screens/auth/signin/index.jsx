d7bfaaac7d5be2f816da3cf9b34e0977d6bacc0b
Vaibhavi Kolloju <vaibhavikolloju@gmail.com>
2024-09-03 17:15:22 +0530

    fixes

--- universal/packages/screens/auth/signin/index.jsx
+++ universal/packages/screens/auth/signin/index.jsx
@@ -7,10 +7,28 @@ import { Text } from "@/components/ui/text";
 import { LinkText } from "@/components/ui/link";
 import { Image } from "@/components/ui/image";
 import Link from "@unitools/link";
-import { FormControl, FormControlError, FormControlErrorIcon, FormControlErrorText, FormControlLabel, FormControlLabelText, } from "@/components/ui/form-control";
+import {
+  FormControl,
+  FormControlError,
+  FormControlErrorIcon,
+  FormControlErrorText,
+  FormControlLabel,
+  FormControlLabelText,
+} from "@/components/ui/form-control";
 import { Input, InputField, InputIcon, InputSlot } from "@/components/ui/input";
-import { Checkbox, CheckboxIcon, CheckboxIndicator, CheckboxLabel, } from "@/components/ui/checkbox";
-import { ArrowLeftIcon, CheckIcon, EyeIcon, EyeOffIcon, Icon, } from "@/components/ui/icon";
+import {
+  Checkbox,
+  CheckboxIcon,
+  CheckboxIndicator,
+  CheckboxLabel,
+} from "@/components/ui/checkbox";
+import {
+  ArrowLeftIcon,
+  CheckIcon,
+  EyeIcon,
+  EyeOffIcon,
+  Icon,
+} from "@/components/ui/icon";
 import { Button, ButtonText, ButtonIcon } from "@/components/ui/button";
 import { Keyboard } from "react-native";
 import { SafeAreaView } from "@/components/ui/safe-area-view";
@@ -22,86 +40,112 @@ import { GoogleIcon } from "./assets/icons/google";
 import { Pressable } from "@/components/ui/pressable";
 import useRouter from "@unitools/router";
 const USERS = [
-    {
-        email: "gabrial@gmail.com",
-        password: "Gabrial@123",
-    },
-    {
-        email: "tom@gmail.com",
-        password: "Tom@123",
-    },
-    {
-        email: "thomas@gmail.com",
-        password: "Thomas@1234",
-    },
+  {
+    email: "gabrial@gmail.com",
+    password: "Gabrial@123",
+  },
+  {
+    email: "tom@gmail.com",
+    password: "Tom@123",
+  },
+  {
+    email: "thomas@gmail.com",
+    password: "Thomas@1234",
+  },
 ];
 const loginSchema = z.object({
-    email: z.string().min(1, "Email is required").email(),
-    password: z.string().min(1, "Password is required"),
-    rememberme: z.boolean().optional(),
+  email: z.string().min(1, "Email is required").email(),
+  password: z.string().min(1, "Password is required"),
+  rememberme: z.boolean().optional(),
 });
 const AuthLayout = (props) => {
-    return (<SafeAreaView className="w-full h-full">
+  return (
+    <SafeAreaView className="w-full h-full">
       <HStack className="w-full h-full bg-background-0">
-        <VStack className="relative w-0 hidden md:flex md:h-full md:min-w-[50%] items-center  justify-center p-7" space="md">
-          <Image source={require("@/assets/auth/radialGradient.png")} className="h-full w-full absolute inset-0 -z-10" alt="Radial Gradient"/>
-          <Image source={require("@/assets/auth/logo.png")} className="h-40 w-40" alt="Gluestack Logo"/>
+        <VStack
+          className="relative w-0 hidden md:flex md:h-full md:min-w-[50%] items-center  justify-center p-7"
+          space="md"
+        >
+          <Image
+            source={require("@/assets/auth/radialGradient.png")}
+            className="h-full w-full absolute inset-0 -z-10"
+            alt="Radial Gradient"
+          />
+          <Image
+            source={require("@/assets/auth/logo.png")}
+            className="h-40 w-40"
+            alt="Gluestack Logo"
+          />
         </VStack>
 
         <VStack className="md:items-center md:justify-center w-full md:max-w-[440px] p-9 md:gap-10 gap-16 md:m-auto md:w-1/2">
           {props.children}
         </VStack>
       </HStack>
-    </SafeAreaView>);
+    </SafeAreaView>
+  );
 };
 const LoginWithLeftBackground = () => {
-    const { control, handleSubmit, reset, formState: { errors }, } = useForm({
-        resolver: zodResolver(loginSchema),
-    });
-    const toast = useToast();
-    const [validated, setValidated] = useState({
-        emailValid: true,
-        passwordValid: true,
-    });
-    const onSubmit = (data) => {
-        const user = USERS.find((element) => element.email === data.email);
-        if (user) {
-            if (user.password !== data.password)
-                setValidated({ emailValid: true, passwordValid: false });
-            else {
-                setValidated({ emailValid: true, passwordValid: true });
-                toast.show({
-                    placement: "bottom right",
-                    render: ({ id }) => {
-                        return (<Toast nativeID={id} variant="accent" action="success">
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(loginSchema),
+  });
+  const toast = useToast();
+  const [validated, setValidated] = useState({
+    emailValid: true,
+    passwordValid: true,
+  });
+  const onSubmit = (data) => {
+    const user = USERS.find((element) => element.email === data.email);
+    if (user) {
+      if (user.password !== data.password)
+        setValidated({ emailValid: true, passwordValid: false });
+      else {
+        setValidated({ emailValid: true, passwordValid: true });
+        toast.show({
+          placement: "bottom right",
+          render: ({ id }) => {
+            return (
+              <Toast nativeID={id} variant="accent" action="success">
                 <ToastTitle>Logged in successfully!</ToastTitle>
-              </Toast>);
-                    },
-                });
-                reset();
-            }
-        }
-        else {
-            setValidated({ emailValid: false, passwordValid: true });
-        }
-    };
-    const [showPassword, setShowPassword] = useState(false);
-    const handleState = () => {
-        setShowPassword((showState) => {
-            return !showState;
+              </Toast>
+            );
+          },
         });
-    };
-    const handleKeyPress = () => {
-        Keyboard.dismiss();
-        handleSubmit(onSubmit)();
-    };
-    const router = useRouter();
-    return (<>
+        reset();
+      }
+    } else {
+      setValidated({ emailValid: false, passwordValid: true });
+    }
+  };
+  const [showPassword, setShowPassword] = useState(false);
+  const handleState = () => {
+    setShowPassword((showState) => {
+      return !showState;
+    });
+  };
+  const handleKeyPress = () => {
+    Keyboard.dismiss();
+    handleSubmit(onSubmit)();
+  };
+  const router = useRouter();
+  return (
+    <>
       <VStack className="md:items-center" space="md">
-        <Pressable onPress={() => {
+        <Pressable
+          onPress={() => {
             router.back();
-        }}>
-          <Icon as={ArrowLeftIcon} className="md:hidden text-background-800" size="xl"/>
+          }}
+        >
+          <Icon
+            as={ArrowLeftIcon}
+            className="md:hidden text-background-800"
+            size="xl"
+          />
         </Pressable>
         <VStack>
           <Heading className="md:text-center" size="3xl">
@@ -112,67 +156,115 @@ const LoginWithLeftBackground = () => {
       </VStack>
       <VStack className="w-full">
         <VStack space="xl" className="w-full">
-          <FormControl isInvalid={!!errors?.email || !validated.emailValid} className="w-full">
+          <FormControl
+            isInvalid={!!errors?.email || !validated.emailValid}
+            className="w-full"
+          >
             <FormControlLabel>
               <FormControlLabelText>Email</FormControlLabelText>
             </FormControlLabel>
-            <Controller defaultValue="" name="email" control={control} rules={{
-            validate: async (value) => {
-                try {
+            <Controller
+              defaultValue=""
+              name="email"
+              control={control}
+              rules={{
+                validate: async (value) => {
+                  try {
                     await loginSchema.parseAsync({ email: value });
                     return true;
-                }
-                catch (error) {
+                  } catch (error) {
                     return error.message;
-                }
-            },
-        }} render={({ field: { onChange, onBlur, value } }) => (<Input>
-                  <InputField placeholder="Enter email" value={value} onChangeText={onChange} onBlur={onBlur} onSubmitEditing={handleKeyPress} returnKeyType="done"/>
-                </Input>)}/>
+                  }
+                },
+              }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input>
+                  <InputField
+                    placeholder="Enter email"
+                    value={value}
+                    onChangeText={onChange}
+                    onBlur={onBlur}
+                    onSubmitEditing={handleKeyPress}
+                    returnKeyType="done"
+                  />
+                </Input>
+              )}
+            />
             <FormControlError>
-              <FormControlErrorIcon as={AlertTriangle}/>
+              <FormControlErrorIcon as={AlertTriangle} />
               <FormControlErrorText>
                 {errors?.email?.message ||
-            (!validated.emailValid && "Email ID not found")}
+                  (!validated.emailValid && "Email ID not found")}
               </FormControlErrorText>
             </FormControlError>
           </FormControl>
           {/* Label Message */}
-          <FormControl isInvalid={!!errors.password || !validated.passwordValid} className="w-full">
+          <FormControl
+            isInvalid={!!errors.password || !validated.passwordValid}
+            className="w-full"
+          >
             <FormControlLabel>
               <FormControlLabelText>Password</FormControlLabelText>
             </FormControlLabel>
-            <Controller defaultValue="" name="password" control={control} rules={{
-            validate: async (value) => {
-                try {
+            <Controller
+              defaultValue=""
+              name="password"
+              control={control}
+              rules={{
+                validate: async (value) => {
+                  try {
                     await loginSchema.parseAsync({ password: value });
                     return true;
-                }
-                catch (error) {
+                  } catch (error) {
                     return error.message;
-                }
-            },
-        }} render={({ field: { onChange, onBlur, value } }) => (<Input>
-                  <InputField type={showPassword ? "text" : "password"} placeholder="Enter password" value={value} onChangeText={onChange} onBlur={onBlur} onSubmitEditing={handleKeyPress} returnKeyType="done"/>
+                  }
+                },
+              }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input>
+                  <InputField
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Enter password"
+                    value={value}
+                    onChangeText={onChange}
+                    onBlur={onBlur}
+                    onSubmitEditing={handleKeyPress}
+                    returnKeyType="done"
+                  />
                   <InputSlot onPress={handleState} className="pr-3">
-                    <InputIcon as={showPassword ? EyeIcon : EyeOffIcon}/>
+                    <InputIcon as={showPassword ? EyeIcon : EyeOffIcon} />
                   </InputSlot>
-                </Input>)}/>
+                </Input>
+              )}
+            />
             <FormControlError>
-              <FormControlErrorIcon as={AlertTriangle}/>
+              <FormControlErrorIcon as={AlertTriangle} />
               <FormControlErrorText>
                 {errors?.password?.message ||
-            (!validated.passwordValid && "Password was incorrect")}
+                  (!validated.passwordValid && "Password was incorrect")}
               </FormControlErrorText>
             </FormControlError>
           </FormControl>
           <HStack className="w-full justify-between ">
-            <Controller name="rememberme" defaultValue={false} control={control} render={({ field: { onChange, value } }) => (<Checkbox size="sm" value="Remember me" isChecked={value} onChange={onChange} aria-label="Remember me">
+            <Controller
+              name="rememberme"
+              defaultValue={false}
+              control={control}
+              render={({ field: { onChange, value } }) => (
+                <Checkbox
+                  size="sm"
+                  value="Remember me"
+                  isChecked={value}
+                  onChange={onChange}
+                  aria-label="Remember me"
+                >
                   <CheckboxIndicator>
-                    <CheckboxIcon as={CheckIcon}/>
+                    <CheckboxIcon as={CheckIcon} />
                   </CheckboxIndicator>
                   <CheckboxLabel>Remember me</CheckboxLabel>
-                </Checkbox>)}/>
+                </Checkbox>
+              )}
+            />
             <Link href="/auth/forgot-password">
               <LinkText className="font-medium text-sm text-primary-700 group-hover/link:text-primary-600">
                 Forgot Password?
@@ -184,26 +276,37 @@ const LoginWithLeftBackground = () => {
           <Button className="w-full" onPress={handleSubmit(onSubmit)}>
             <ButtonText className="font-medium">Log in</ButtonText>
           </Button>
-          <Button variant="outline" action="secondary" className="w-full gap-1" onPress={() => { }}>
+          <Button
+            variant="outline"
+            action="secondary"
+            className="w-full gap-1"
+            onPress={() => {}}
+          >
             <ButtonText className="font-medium">
               Continue with Google
             </ButtonText>
-            <ButtonIcon as={GoogleIcon}/>
+            <ButtonIcon as={GoogleIcon} />
           </Button>
         </VStack>
-        <HStack className="self-center ">
+        <HStack className="self-center" space="sm">
           <Text size="md">Don't have an account?</Text>
           <Link href="/auth/signup">
-            <LinkText className="font-medium text-primary-700 ml-1 group-hover/link:text-primary-600  group-hover/pressed:text-primary-700" size="md">
+            <LinkText
+              className="font-medium text-primary-700 ml-1 group-hover/link:text-primary-600  group-hover/pressed:text-primary-700"
+              size="md"
+            >
               Sign up
             </LinkText>
           </Link>
         </HStack>
       </VStack>
-    </>);
+    </>
+  );
 };
 export const SignIn = () => {
-    return (<AuthLayout>
+  return (
+    <AuthLayout>
       <LoginWithLeftBackground />
-    </AuthLayout>);
+    </AuthLayout>
+  );
 };
