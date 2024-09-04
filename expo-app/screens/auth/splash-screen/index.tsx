55e7ec70d5859386faa7280a576f9fbd78022a6f
Vaibhavi Kolloju <vaibhavikolloju@gmail.com>
2024-09-04 12:45:09 +0530

    fix: dark theme ui

--- expo-app/screens/auth/splash-screen/index.tsx
+++ expo-app/screens/auth/splash-screen/index.tsx
@@ -1,18 +1,28 @@
 import { VStack } from "@/components/ui/vstack";
 import { Button, ButtonText } from "@/components/ui/button";
 import { Icon } from "@/components/ui/icon";
-import { GluestackIcon } from "./assets/icons/gluestack-icon";
+import {
+  GluestackIcon,
+  GluestackIconDark,
+} from "./assets/icons/gluestack-icon";
+import { useColorScheme } from "nativewind";
+
 import useRouter from "@unitools/router";
 import { AuthLayout } from "../layout";
 
-const SplashScreenWithLeftBackground = () => {
+const SplashScreenWithLeftBackground = ({}: {}) => {
   const router = useRouter();
+  const { colorScheme } = useColorScheme();
   return (
     <VStack
       className="w-full max-w-[440px] items-center h-full justify-center"
       space="lg"
     >
-      <Icon as={GluestackIcon} className="w-[219px] h-10" />
+      {colorScheme === "dark" ? (
+        <Icon as={GluestackIconDark} className="w-[219px] h-10" />
+      ) : (
+        <Icon as={GluestackIcon} className="w-[219px] h-10" />
+      )}
       <VStack className="w-full" space="lg">
         <Button
           className="w-full"
