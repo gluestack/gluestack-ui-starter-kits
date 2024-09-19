f96385b620db8dcf961bd832dbe1aef95cc138a0
Vaibhavi Kolloju <vaibhavikolloju@gmail.com>
2024-09-19 16:29:27 +0530

    fix: workflow

--- expo-app/jest.config.js
+++ expo-app/jest.config.js
@@ -1,7 +1,13 @@
 module.exports = {
+  preset: "jest-expo",
   roots: ["<rootDir>/__tests__"],
-  testEnvironment: "node",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  testEnvironment: "jsdom",
   testPathIgnorePatterns: ["/node_modules/", "__test__"],
+  transformIgnorePatterns: [
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
+  ],
+  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
   collectCoverage: true,
   collectCoverageFrom: ["src/**/*.{js,jsx}"],
   reporters: ["default", "jest-junit"],
