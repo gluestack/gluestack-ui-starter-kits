7029e4c461ff5081b20e9d862738f3b1fd620eb8
viraj-10 <virajjoshi132@gmail.com>
2024-11-29 15:48:49 +0530

    fix: init of next

--- next/app/layout.tsx
+++ next/app/layout.tsx
@@ -1,4 +1,4 @@
-"use client";;
+"use client";
 import { Inter } from "next/font/google";
 import "@/styles/global.css";
 import StyledJsxRegistry from "./registry";
@@ -14,7 +14,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className} style={{ display: "flex" }}>
         <StyledJsxRegistry>
-          <GluestackUIProvider mode="light" >{children}</GluestackUIProvider>
+          <GluestackUIProvider mode="light">{children}</GluestackUIProvider>
         </StyledJsxRegistry>
       </body>
     </html>
